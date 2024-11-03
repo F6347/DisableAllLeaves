dd01da77441276046fdf90f1024d10ac1b698378
F6347 <103911667+F6347@users.noreply.github.com>
2024-11-03 08:56:02 -0500

    Update PluginInfo.cs
    Update version

--- DisableAllLeaves/PluginInfo.cs
+++ DisableAllLeaves/PluginInfo.cs
@@ -1,12 +1,12 @@
-﻿namespace DisableAllLeaves
+namespace DisableAllLeaves
 {
     /// <summary>
     /// This class is used to provide information about your mod to BepInEx.
     /// </summary>
     internal class PluginInfo
     {
-        public const string GUID = "com.author.gorillatag.disableallleaves";
+        public const string GUID = "com.f6347.gorillatag.disableallleaves";
         public const string Name = "DisableAllLeaves";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
     }
 }
